301315f374da7cc32d216e796fefb153a9bf154c
andrei-stoica26 <andreistoica@foxmail.com>
2025-07-23 10:42:15 +0800

    Added verbosity option in marker overlap function

--- R/assess_overlaps.R
+++ R/assess_overlaps.R
@@ -6,6 +6,7 @@
 #' @param markerList2 List of data frames.
 #' @inheritParams markerSetsPhyper
 #' @param pvalThr p-value threshold.
+#' @param verbose Whether the output should be verbose.
 #'
 #' @return A data frame.
 #'
@@ -15,16 +16,25 @@ markerListPhyper <- function(markerList1, markerList2, nGenes,
                              colStr = 'avg_log2FC',
                              isHighTop = TRUE,
                              extraCutoff = 0,
-                             pvalThr = 0.05){
+                             pvalThr = 0.05,
+                             verbose = TRUE){
     df <- pairDF(names(markerList1), names(markerList2))
     df$pval <- apply(df, 1,
-                     function(x) markerSetsPhyper(
-                         markerList1[[x[1]]],
-                         markerList2[[x[2]]],
-                         nGenes,
-                         colStr,
-                         isHighTop,
-                         extraCutoff)
+                     function(x){
+                         markerNames1 <- x[1]
+                         markerNames2 <- x[2]
+                         if (verbose)
+                             message('Assessing overlap between marker sets: ',
+                                     markerNames1, ' and ', markerNames2, '...')
+                         markerSetsPhyper(
+                             markerList1[[markerNames1]],
+                             markerList2[[markerNames2]],
+                             nGenes,
+                             colStr,
+                             isHighTop,
+                             extraCutoff)
+
+                         }
                      )
     df <- byCorrectDF(df, pvalThr)
     return(df)
