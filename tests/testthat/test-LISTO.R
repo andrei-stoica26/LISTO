0300579f36914d5d5219bc20fe87691e2931c559
andrei-stoica26 <andreistoica@foxmail.com>
2025-09-30 14:53:44 +0800

    Added some tests for enrichment functions

--- tests/testthat/test-LISTO.R
+++ tests/testthat/test-LISTO.R
@@ -3,3 +3,12 @@ test_that("pOverlapMNk works", {
         pOverlapMNk(300, 70, 110, i), numeric(1)))
     expect_equal(res, 1, tolerance=0.0001)
 })
+
+test_that("gene enrichment functions work", {
+    m <- genesER(c('AURKA', 'TOP2A', 'CENPF', 'PTTG2', 'MKI67', 'BIRC5',
+                   'RRM2'),
+                 'human')
+    expect_true('chromosome segregation' %in% m@result$Description)
+    expect_equal(termGenes(m, 'chromosome segregation', 'meiosis I'),
+                 c('BIRC5', 'CENPF', 'MKI67'))
+})
